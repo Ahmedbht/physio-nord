ad7378df149d46674d247789e0bce3c123b1b0cc
Ahmed <ahmedbouhtit8@gmail.com>
2026-06-28 12:49:03 +0100

    edit services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,48 +8,49 @@ import electro from "../electro.jpg";
 
 function Services(){
     const services=[
-        {
-            id: 1,
-            icon: <Activity size={36}/>,
-            image:kine,
-            title:"Kinésithérapie",
-            description: "Traitement des troubles musculosquelettiques par des techniques manuelles et des exercices thérapeutiques."
-        },
-        {
-            id: 2,
-            icon: <Heart size={36}/>,
-            image:cardio,
-            title:"Rééducation Cardiaque",
-            description: "Programme de rééducation adapté aux patients souffrant de maladies cardiovasculaires."
-        },
-        {
-            id: 3,
-            icon: <Bone size={36}/>,
-            image:ortho,
-            title:"Rééducation Orthopédique",
-            description: "Prise en charge des fractures, entorses et pathologies articulaires après chirurgie ou traumatisme."
-        },
-        {
-            id: 4,
-            icon: <Brain size={36}/>,
-            image:neuro,
-            title:"Rééducation Neurologique",
-            description: "Accompagnement des patients atteints de pathologies neurologiques comme l'AVC."
-        },
-        {
-            id: 5,
-            icon: <Baby size={36}/>,
-            image:pediatric,
-            title:"Kinésithérapie Pédiatrique",
-            description: "Soins spécialisés pour les enfants souffrant de troubles moteurs ou de retards de développement."
-        },
-        {
-            id: 6,
-            icon: <Zap size={36}/>,
-            image:electro,
-            title:"Électrothérapie",
-            description: "Utilisation des courants électriques pour soulager la douleur et stimuler la récupération musculaire."
-        }
+       {
+    id: 1,
+    icon: <Activity size={36}/>,
+    image: kine,
+    title: "Rééducation Maxillo-faciale",
+    description: "Prise en charge des troubles de la mâchoire et des muscles faciaux."
+},
+{
+    id: 2,
+    icon: <Heart size={36}/>,
+    image: cardio,
+    title: "Rééducation Neuro Vestibulaire",
+    description: "Traitement des troubles de l'équilibre et des vertiges liés au système vestibulaire."
+},
+{
+    id: 3,
+    icon: <Bone size={36}/>,
+    image: ortho,
+    title: "Neuro-Réhabilitation Pédiatrique",
+    description: "Rééducation neurologique spécialisée pour les enfants présentant des troubles moteurs."
+},
+{
+    id: 4,
+    icon: <Brain size={36}/>,
+    image: neuro,
+    title: "Kinésithérapie Médico-Sportive",
+    description: "Prise en charge des blessures sportives et optimisation des performances athlétiques."
+},
+{
+    id: 5,
+    icon: <Baby size={36}/>,
+    image: pediatric,
+    title: "Rééducation Périnéale",
+    description: "Rééducation du plancher pelvien pour traiter les troubles urinaires et post-partum."
+},
+{
+    id: 6,
+    icon: <Zap size={36}/>,
+    image: electro,
+    title: "Réadaptation Cardio-Respiratoire",
+    description: "Programme de rééducation pour les pathologies cardiaques et respiratoires."
+},
+
     ];
 
     return(
